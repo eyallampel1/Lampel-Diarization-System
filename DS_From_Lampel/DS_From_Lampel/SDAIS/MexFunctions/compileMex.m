3cf24b6dbcb0d1d0dd5a3fd092086f10116897a8
eyallampel1 <eyal.lampel@gmail.com>
2016-02-16 10:44:43 +0200

    Speaker Diarization System By Lampel

--- DS_From_Lampel/DS_From_Lampel/SDAIS/MexFunctions/compileMex.m
+++
@@ -1,4 +0,0 @@
-[FileName,PathName] = uigetfile('*.cpp');
-functionName = ['''' PathName FileName ''''];
-eval(['mex -I''C:\Program Files\Intel\ComposerXE-2011\mkl\include\'' -L''C:\Program Files\Intel\ComposerXE-2011\mkl\tools\builder'' -lmkl_custom ' functionName]);
-
